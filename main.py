be975626ac9bc83afd7f418484299ba46cc7546c
nataliariverax <nataliarivera@berkeley.edu>
2025-08-09 17:08:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,12 @@
 import numpy as np
 import matplotlib as plt
 
+#define class structure
 def Class ():
   def __init__(self):
     self.x = 5
+
+#E coli walk function
+def EcoliWalk(N = 1000, #speed?, #steps?):
+              t = 0
+              dt = 
