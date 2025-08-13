9a3c5cbdf12cf98777b08934d64a128bb1585ff2
Andy <andyho66@gmail.com>
2025-08-12 20:14:10 -0700

    Altered runEcoliWalk_F1 function so a plot for each E.coli will appear

--- main.py
+++ main.py
@@ -324,26 +324,17 @@ class Ecoli_Walk_Simulation():
       self.all_Ecoli_x[i] = Ecoli_x
       self.all_Ecoli_y[i] = Ecoli_y
 
+      #Reshape Ecoli arrays for plotting (from 2D to 1D) by stacking column(i)
+      #below column(i-1):
+      plotX = np.reshape(Ecoli_x.transpose(),(iterationCount*tumblesPlusRun,))
+      plotY = np.reshape(Ecoli_y.transpose(),(iterationCount*tumblesPlusRun,))
+
+
+      #Use plot_Ecloi function to genatre the plot
+      plot_Ecoli = self.plot_Ecoli
+      plot_Ecoli(plotX, plotY)
     #__________________________________________________________________________#
 
-
-
-
-
-    #Reshape Ecoli arrays for plotting (from 2D to 1D) by stacking column(i)
-    #below column(i-1):
-    plotX = np.reshape(Ecoli_x.transpose(),(iterationCount*tumblesPlusRun,))
-    plotY = np.reshape(Ecoli_y.transpose(),(iterationCount*tumblesPlusRun,))
-
-
-    #Use plot_Ecloi function to genatre the plot
-    plot_Ecoli = self.plot_Ecoli
-    plot_Ecoli(plotX, plotY)
-
-
-
-
-    #__________________________________________________________________________#
 
   def plot_Ecoli(self, plotX, plotY):
 
@@ -450,75 +441,4 @@ class Ecoli_Walk_Simulation():
 
     plt.tight_layout()
     plt.show()
-
-
-
-
-
-
-
-
-
-
-
-#___________________________Previous Code___________________________#
-#use gaussian/normal distribution to define continuous concentration field representing food source
-#gradient maximum = (0,0) = highest food concentration
-# def food_source(x, y, x0 = 0, y0 = 0, sigma = 3):
-#   return np.exp(-((x-x0)**2 + (y-y0)**2) / (2 * sigma**2))
-
-#create concentration field gradient, 3 profiles total
-#picked numbers
-# def conc_exponential(x, y, alpha=0.5):
-#   r = np.sqrt(x*x + y*y)
-#   return np.exp(-alpha * r)
-
-# def conc_linear(x, y, k = 0.2):
-#   return k * x
-
-#not sure if this is the best practice but I am creating a constant
-#i.e right now, food_source is selected --> gaussian/normal distribution
-# FIELD = food_source
-
-#helper function to apply FIELD, can put this in main too
-# def field_function(profile):
-#     return np.vectorize(lambda x, y: profile(x, y))
-
-
-  # def __init__(self):
-  #   self.x = np.random.uniform(-10, 10)  #randomize initial position
-  #   self.y = np.random.uniform(-10, 10)
-  #   self.time_step = 0
-  #   self.Ecolipath.append(np.array([self.x, self.y], float))    #store positions in list, starting w/ initial position
-
-  # def tumble(self):
-  #   for i in range(4):
-  #     self.time_step += 1
-  #     if self.time_step % 4 == 0:
-  #       step_x = np.random.normal(-1.0, 1.0)
-  #       step_y = np.random.normal(-1.0, 1.0)       #randomize steps
-  #       self.x += step_x
-  #       self.y += step_x              #update current position
-  #       self.Ecolipath.append(np.array([self.x, self.y], float))  #changed to 2D
-
-  # def run():
-  #   pass
-
-  # def cycle():
-  #   pass
-
-  # def plot_traj(field, cell, L = 15):
-  #   pass
-
-  # def plot_histograms(dist_by_time):
-
-  #   pass
-
-  # #run whole
-  # if __name__ == "__main__":
-  #   field = field_function(FIELD) #calling helper function depending on profile
-
-
-
-
-
+    
