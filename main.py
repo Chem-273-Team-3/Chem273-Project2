0179264e61017e961905e1d61072ebf0855148c6
Andy <andyho66@gmail.com>
2025-08-14 12:24:32 -0700

    Added optimization techniques and adjusted plot to show after every 100 time steps

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 
-def randSteps(stepSize_mu: float = 0.4,
-              stepSize_std: float = 0.04,
+def randSteps(stepSize_mu: np.array = np.ones((2,1)),
+              stepSize_std: np.array = np.ones((2,1)),
               N_steps: int = 4):
+
 
   """
   This function generates a random step <x,y> vector, where the user
@@ -21,25 +22,21 @@ def randSteps(stepSize_mu: float = 0.4,
   Outputs:
       x,y np.arrays((N_steps,1))
   """
-
-
-
-  #Randomly pick the distance to step "N_steps" times from a normal distribution:
-  rStepSize = np.random.normal(stepSize_mu,stepSize_std,N_steps)
-
-  #Randomly pick an angle in radians (0 to 2pi) from a uniform distrubution:
-  rStepAngle = np.random.uniform(0,2*np.pi,N_steps)
-
-
   #Initialize x,y columns with one mroe row than steps
+
   x = np.zeros((N_steps+1,1))
   y = np.zeros((N_steps+1,1))
 
-  #Convert length,angle cooridinates to x,y cooridinates:
-  #Add all previous steps the current step to show walking affect:
-  #First row remains 0
-  x[1:,0] = np.cumsum(rStepSize*np.cos(rStepAngle))
-  y[1:,0] = np.cumsum(rStepSize*np.sin(rStepAngle))
+  x_tumbles = np.random.normal(stepSize_mu[0],stepSize_std[0],N_steps)
+  y_tumbles = np.random.normal(stepSize_mu[1],stepSize_std[1],N_steps)
+
+  x_tumbles *= np.random.choice([-1,1],size = np.shape(x_tumbles))
+  y_tumbles *= np.random.choice([-1,1],size = np.shape(y_tumbles))
+
+
+  x[1:,0] = np.cumsum(x_tumbles)
+  y[1:,0] = np.cumsum(y_tumbles)
+
 
 
   return x,y
@@ -69,6 +66,9 @@ class Ecoli_Walk_Simulation():
               stepSize_std: float = 0.5,
               grad_Est: bool = True,
               plotLevels: int = 20,
+              eps_learning_rate: float = 0.8,
+              run_to_tumble_stepMu_Ratio: int = 5,
+              tumbleMu_to_tumbleSTD_Ratio: float = 100,
               N: int = 10):
 
 
@@ -86,6 +86,9 @@ class Ecoli_Walk_Simulation():
     self.stepSize_std = stepSize_std
     self.grad_Est = grad_Est
     self.plotLevels = plotLevels
+    self.eps_learning_rate = eps_learning_rate
+    self.run_to_tumble_stepMu_Ratio  = run_to_tumble_stepMu_Ratio
+    self.tumbleMu_to_tumbleSTD_Ratio = tumbleMu_to_tumbleSTD_Ratio
     self.N = N
     self.all_Ecoli_x = np.zeros((N, tumbles + 1, iterationCount))
     self.all_Ecoli_y = np.zeros((N, tumbles + 1, iterationCount))
@@ -145,75 +148,19 @@ class Ecoli_Walk_Simulation():
     FoodLoc_F1 = self.FoodLoc_F1
 
 
-    #Take partial derivative in x and y directions
-    #In question 1:
-    Q1 = 1
-    if Q1 == 1:
-        #Evaluate the difference in Z at time step t+4dt vs t
-        dA_dX = FoodLoc_F1(x=X[-1],y=Y[-1]) - FoodLoc_F1(x=X[0],y=Y[0])
-        dA_dY = FoodLoc_F1(x=X[-1],y=Y[-1]) - FoodLoc_F1(x=X[0],y=Y[0])
-    else:
-        dA_dX = FoodLoc_F1(x=X[-1],y=Y[0]) - FoodLoc_F1(x=X[0],y=Y[0])
-        dA_dY = FoodLoc_F1(x=X[0],y=Y[-1]) - FoodLoc_F1(x=X[0],y=Y[0])
-
-
-    #In question 2:
-    Q2 = 1
-    if Q2 == 1:
-        dX = X[-1] - X[0] #Take the difference of X before and after random steps
-        dY = Y[-1] - Y[0] #Take the difference of Y before and after random steps
-    else:
-        dX = 2*(X[-1] - X[0]) #Take the 2 times the difference of X before and after random steps
-        dY = 2*(Y[-1] - Y[0]) #Take the 2 times the difference of Y before and after random steps
-
-
-    #Convert partial derivatives to a np.array ((2,1))
-    gradA = np.array([[dA_dX/dX],[dA_dY/dY]])
-
-    #Determine the Euclidian Norm of gradA for to control scaling
-    normFactor = np.sqrt(np.sum(gradA**2))
-
-    #Scale gradA
-    gradA /= normFactor
-
-    return gradA
-
-  def grad_F1(self, X, Y):
-    """
-    Knowing F1, take the actual partial derivative evaluated after "N_steps"
-    random steps with respect to both the X and Y directions
-
-    Inputs:
-        X,Y = np.array((N_steps+1 , 1))
-            where:
-                first row = initial position OR position after run step
-                last row  = position after "N_steps" random steps
-
-    Output:
-        gradA = np.array((2,1))
-            gradA[0] = partial derivative with respect to the x-direction (normalized distance)
-            gradA[1] = partial derivative with respect to the y-direction (normalized distance)
-
-    """
-
     A = self.A
-    a = self.A
+    a = self.a
     B = self.B
-    b = self.B
+    b = self.b
+
+
 
     dF1_dX = 2*A*(X[-1] - a) #Partial derivative with respect to X
 
     dF1_dY = 2*B*(Y[-1] - b) #Partial derivative with respect to Y
 
 
-    #Convert partial derivatives to a np.array ((2,1)):
-    gradF1= np.array([[dF1_dX],[dF1_dY]])
-    normFactor = np.sqrt(np.sum(gradF1**2)) #Euclidian Norm
-
-    #Scale gradF1
-    gradF1 /= normFactor
-
-    return gradF1
+    return dF1_dX, dF1_dY
 
 
   def runEcoliWalk_F1(self):
@@ -236,7 +183,7 @@ class Ecoli_Walk_Simulation():
     #Bring in F1 functions from self
     grad_F1_estimation = self.grad_F1_estimation
 
-    grad_F1 = self.grad_F1
+    # grad_F1 = self.grad_F1
 
 
 
@@ -253,26 +200,41 @@ class Ecoli_Walk_Simulation():
       Ecoli_x = X_init*np.ones((tumblesPlusRun , iterationCount))
 
       Ecoli_y = Y_init*np.ones((tumblesPlusRun , iterationCount))
+              ###Learning Rates###
+
+      runStep_learningRate_eps = self.eps_learning_rate*np.ones((2,1)) #set the run step length for (x,y)
+
+      #set the tumble step length for (x,y)
+      tumbleStep_Mu = runStep_learningRate_eps/self.run_to_tumble_stepMu_Ratio
+
+      #set the tumble step variance for (x,y)
+      tumbleStep_std = tumbleStep_Mu  /self.tumbleMu_to_tumbleSTD_Ratio
 
 
+      #__________________________________________________________________________#
 
-
-
-      #steps_X = the np.cumsum of Random steps (tumble) in the X-direction
-      #steps_Y = the np.cumsum of Random steps (tumble) in the Y-direction
-      steps_x, steps_y = randSteps(stepSize_mu = self.stepSize_mu,
-                                    stepSize_std = self.stepSize_std,
+      tumbles_x, tumbles_y = randSteps(stepSize_mu = tumbleStep_Mu,
+                                    stepSize_std = tumbleStep_std ,
                                     N_steps = self.tumbles)
 
       #__________________________________________________________________________#
 
       ###Initial Steps###
 
-      Ecoli_x[:,0] += steps_x[:,0] #Add the cumsum to X_init
+      Ecoli_x[:,0] += tumbles_x[:,0] #Add the cumsum to X_init
 
-      Ecoli_y[:,0] += steps_y[:,0] #Add the cumsum to Y_init
+      Ecoli_y[:,0] += tumbles_y[:,0] #Add the cumsum to Y_init
 
       #__________________________________________________________________________#
+      #Optimizer Terms
+      Gx=0
+      Gy=0
+      Gx_2 = 0
+      Gy_2 = 0
+      runX = 0
+      runY = 0
+      RMSP_Gx_2 = 0
+      RMSP_Gy_2 = 0
 
 
       ###Loop through remaining steps with a similar logic to the initial step###
@@ -285,67 +247,107 @@ class Ecoli_Walk_Simulation():
         prev_X = Ecoli_x[:,runIter-1] #save the previous run+tumble steps
         prev_Y = Ecoli_y[:,runIter-1] #save the previous run+tumble steps
 
-
-
-        #Determine the run vector direction and multiply the length of the vector by runStep_Magnitude_eps
-            #User choice of using estimated partial derivatives or actual partial derivatives
-        if self.grad_Est == True:
-
-            runXY = runStep_Magnitude_eps * grad_F1_estimation( X = prev_X , Y = prev_Y)
-
-        else:
-
-            runXY = runStep_Magnitude_eps * grad_F1( X = prev_X , Y = prev_Y )
-
+        runX, runY = grad_F1_estimation(X = prev_X, Y = prev_Y)
 
         #______________________________________________________________________#
+        ###Momentum:
+        #Momentum_decay = 0.9
+        #Gx = Momentum_decay*Gx + runX
+        #Gy = Momentum_decay*Gy + runY
 
+        ####Replace with Adam Momentum:
+        Momentum_decay = 0.9
+        Gx = Momentum_decay*Gx + runX*(1-Momentum_decay)
+        Gy = Momentum_decay*Gy + runY*(1-Momentum_decay)
+
+        ###Adagrad:
+        #Gx_2 += runX**2
+        #Gy_2 += runY**2
+
+        #runStep_learningRate_eps[0] = self.eps_learning_rate/(np.sqrt(Gx_2) + 1e-8)
+        #runStep_learningRate_eps[1] = self.eps_learning_rate/(np.sqrt(Gy_2) + 1e-8)
+
+        ####Replace Adagrad with RMSProp
+        RMSP_decayRate = 0.9999
+
+        RMSP_Gx_2 = (1-RMSP_decayRate) * runX**2  +  RMSP_decayRate * RMSP_Gx_2
+        RMSP_Gy_2 = (1-RMSP_decayRate) * runY**2  +  RMSP_decayRate * RMSP_Gy_2
+
+
+        runStep_learningRate_eps[0] = self.eps_learning_rate/(np.sqrt(RMSP_Gx_2) + 1e-8)
+        runStep_learningRate_eps[1] = self.eps_learning_rate/(np.sqrt(RMSP_Gy_2) + 1e-8)
 
         #Determine first step (similar to init_X, init_Y before loop)
 
-        #Set all of the current step of Ecoli_x, Ecoli_y =
-        #to the value of (the last tumble step) + (the run step)
+        #Determine first step (similar to init_X, init_Y before loop)
 
-        Ecoli_x[:,runIter] = prev_X[-1] + runXY[0]
+        ###NO L1L2:
+        Ecoli_x[:,runIter] = (prev_X[-1] + runStep_learningRate_eps[0]*Gx) * np.ones(np.shape(prev_X))
 
-        Ecoli_y[:,runIter] = prev_Y[-1] + runXY[1]
+        Ecoli_y[:,runIter] = (prev_Y[-1] + runStep_learningRate_eps[1]*Gy) * np.ones(np.shape(prev_X))
+
+        ####L1L2 Test:
+        #L1:
+        #dX = prev_X[-1] - prev_X[0]
+        #dY = prev_Y[-1] - prev_Y[0]
+        #dXdx = np.sign(dX)
+        #dYdy =  np.sign(dY)
+
+        #L1x = 1e-8 * np.abs(dX)
+        #L1y = 1e-8 * np.abs(dX)
+
+        #L2:
+        #dX2dx = dX**2 / dX
+        #dY2dy = dY**2 / dY
+
+        #L2x = 1e-4 * np.abs(dX)
+        #L2y = 1e-4 * np.abs(dX)
 
 
+        #Ecoli_x[:,runIter] = (prev_X[-1] + runStep_learningRate_eps[0]*Gx \
+        #                      + runStep_learningRate_eps[0]*L1x*dXdx \
+        #                          + runStep_learningRate_eps[0]*L2x*dX2dx) \
+        #    * np.ones(np.shape(prev_X))
 
-        #steps_X = the np.cumsum of Random steps (tumble) in the X-direction
-        #steps_Y = the np.cumsum of Random steps (tumble) in the Y-direction
-        steps_x, steps_y = randSteps(stepSize_mu=.6)#NEEDS UPDATED
+        #Ecoli_y[:,runIter] = (prev_Y[-1] + runStep_learningRate_eps[1]*Gy \
+        #                      + runStep_learningRate_eps[1]*L1y*dYdy \
+        #                          + runStep_learningRate_eps[1]*L2y*dY2dy) \
+        #    * np.ones(np.shape(prev_X))
 
-        Ecoli_x[:,runIter] += steps_x[:,0]
+        #Update Magnitude of random steps:
 
-        Ecoli_y[:,runIter] += steps_y[:,0]
+        tumbleStep_Mu = runStep_learningRate_eps*np.sqrt(runX**2 + runY**2)/self.run_to_tumble_stepMu_Ratio
+        tumbleStep_std = tumbleStep_Mu / self.tumbleMu_to_tumbleSTD_Ratio
+
+        tumbles_x, tumbles_y = randSteps(stepSize_mu = tumbleStep_Mu,
+                                      stepSize_std = tumbleStep_std ,
+                                      N_steps = self.tumbles)
+
+        #______________________________________________________________________#
+
+        Ecoli_x[:,runIter] += tumbles_x[:,0]
+
+        Ecoli_y[:,runIter] += tumbles_y[:,0]
+
+        if runIter % 100 == 0:
+            #Reshape Ecoli arrays for plotting (from 2D to 1D) by stacking column(i)
+            #below column(i-1):
+            plotX = np.reshape(Ecoli_x.transpose(),(iterationCount*tumblesPlusRun,))
+            plotY = np.reshape(Ecoli_y.transpose(),(iterationCount*tumblesPlusRun,))
+
+            #Use plot_Ecloi function to genatre the plot
+            plot_Ecoli = self.plot_Ecoli
+            plot_Ecoli(plotX, plotY)
 
       #store x,y values for each Ecoli
       self.all_Ecoli_x[i] = Ecoli_x
       self.all_Ecoli_y[i] = Ecoli_y
 
-      #Reshape Ecoli arrays for plotting (from 2D to 1D) by stacking column(i)
-      #below column(i-1):
-      plotX = np.reshape(Ecoli_x.transpose(),(iterationCount*tumblesPlusRun,))
-      plotY = np.reshape(Ecoli_y.transpose(),(iterationCount*tumblesPlusRun,))
-
-
-      #Use plot_Ecloi function to genatre the plot
-      plot_Ecoli = self.plot_Ecoli
-      plot_Ecoli(plotX, plotY)
-    #__________________________________________________________________________#
-
-
   def plot_Ecoli(self, plotX, plotY):
-
-
 
     FoodLoc_F1 = self.FoodLoc_F1
 
-
-
-
-
+    #Determine Axes min and max for plot:
     X_min = np.min(plotX)
 
     Y_min = np.min(plotY)
@@ -354,38 +356,34 @@ class Ecoli_Walk_Simulation():
 
     Y_max = np.max(plotY)
 
-
-
     X_spacer = (X_max-X_min)/20
 
     Y_spacer = (Y_max-Y_min)/20
 
+      #__________________________________________________________________________#
 
+    iteration_size = self.tumbles +1
 
+    iter_start_X = plotX[::iteration_size]
+    iter_start_Y = plotY[::iteration_size]
 
-
-
-
+    #__________________________________________________________________________#
+    #Plotting for food source function in 2D:
     x = np.linspace(X_min - X_spacer, X_max + X_spacer, 1000)
-
     y = np.linspace(Y_min - Y_spacer, Y_max + Y_spacer, 1000)
 
-
-
     X, Y = np.meshgrid(x,y)
-
-
-
     Z = FoodLoc_F1(x = X, y = Y)
-
-
+    #__________________________________________________________________________#
 
 
     #Plot Result:
 
-    plt.contourf(X,Y,Z, levels = self.plotLevels) #Gradient
+    plt.contourf(X,Y,Z, levels = self.plotLevels) #Plotting for food source function in 2D:
 
-    plt.plot(plotX, plotY, color = 'r',marker = 'o', markersize = 3) #Ecoli
+    plt.plot(plotX, plotY, color = 'r',marker = 'o', markersize = 3) #Ecoli walk
+
+    plt.scatter(iter_start_X,iter_start_Y, color = 'k', marker = '*', s = 100)
 
     plt.show()
 
@@ -441,4 +439,7 @@ class Ecoli_Walk_Simulation():
 
     plt.tight_layout()
     plt.show()
-    
+
+
+test = Ecoli_Walk_Simulation(a = 0, b = 0, iterationCount=1000, N = 10)
+test.runEcoliWalk_F1()
